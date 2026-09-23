b6d5f1582a8454bfc1dcbc38c7b1844b9f1781bd
Breno Campos Franco <canaldarkbreno@gmail.com>
2026-09-22 21:04:25 -0300

    refatoração .env no docker

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
-load_dotenv("variaveis.env")
+load_dotenv(".env")
 
 def get_connect_dw():
     conn = psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD")
     )
     return conn
